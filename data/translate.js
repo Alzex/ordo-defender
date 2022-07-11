7a1596799d9d93c2ebf142a1b9ed28f20db4d339
Alzex <41523683+Alzex@users.noreply.github.com>
2022-07-11 13:26:01 +0300

    Fix

--- data/translate.js
+++ data/translate.js
@@ -2,14 +2,10 @@ const ru = require('./translates/ru.json');
 const en = require('./translates/en.json');
 const uk = require('./translates/uk.json');
 
-const translates = {
-    ru: ru,
-    en: en,
-    uk: uk
-}
+const translates = {ru, en, uk};
 
 const translateData = {
-    getTranslate: (code) => {
+    getTranslate(code) {
         return Object.keys(translates).includes(code) ? translates[code] : translates.en;
     }
 }
