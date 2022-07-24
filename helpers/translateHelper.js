6208e0e50fcf302cfa6aefaf700a7189b12a58fe
Alzex <41523683+Alzex@users.noreply.github.com>
2022-07-24 17:07:47 +0300

    Fixed minor problems

--- helpers/translateHelper.js
+++ helpers/translateHelper.js
@@ -3,6 +3,9 @@ const translateHelper = {
         return text.replaceAll('{user}', `<b><a href="tg://user?id=${user.id}">${user.first_name}</a></b>`);
     },
     multiParseNames: (text, ...users) => {
+        if (!text) {
+            return 'TextError';
+        }
         let txt = text;
         for (const user of users) {
             txt = txt.replace(`{user${users.indexOf(user)}}`, `<b><a href="tg://user?id=${user.id}">${user.first_name}</a></b>`);
